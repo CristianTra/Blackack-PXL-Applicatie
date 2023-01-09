cca338a435e71c5022a343e6c1d81c86d5d8032d
Cristian Travaglini <cristian.travaglini@student.pxl.be>
2023-01-09 23:27:23 +0100

    klr

--- Blackjack PXL Applicatie/Blackjack PXL Applicatie/MainWindow.xaml.cs
+++ Blackjack PXL Applicatie/Blackjack PXL Applicatie/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,36 +21,83 @@ namespace Blackjack_PXL_Applicatie
     /// </summary>
     public partial class MainWindow : Window
     {
+        string Tester = "Welkom";
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        private void btnDeel_Click(object sender, RoutedEventArgs e)
+        
+        private async void btnDeel_Click(object sender, RoutedEventArgs e)
         {
             var randomnr = new Random();
             int randomnr1 = randomnr.Next(1, 11);
             string rndmnr1 = Convert.ToString(randomnr1);
 
-            var randomnr01 = new Random();
-            int randomnr001 = randomnr01.Next(1, 11);
-            string rndmnr01 = Convert.ToString(randomnr1);
 
+            int randomnr001 = randomnr.Next(1, 11);
+            string rndmnr01 = Convert.ToString(randomnr001);
+
+            int randomnrdealer = randomnr.Next(1, 11);
+            string rndmnrdealer = Convert.ToString(randomnrdealer);
+            
             lblTotaalSpeler.Content = randomnr001 + randomnr1;
 
+            string Space = Environment.NewLine;
+
+            var rndmsymb = new Random();
+            string[] randomsymbolen = { "Klaver", "Ruit", "Schoppen", "Harten" };
+            string randomsymbolen1 = randomsymbolen[rndmsymb.Next(randomsymbolen.Length)]; 
+            string randomkaart = (randomsymbolen1 + " " + rndmnr1);
+
+
+            string[] randomsymbolen0 = { "Klaver", "Ruit", "Schoppen", "Harten" };
+            string randomsymbolen01 = randomsymbolen0[rndmsymb.Next(randomsymbolen0.Length)];
+            string randomkaart0 = (randomsymbolen01 + " " + rndmnr01);
+
+
+            string[] randomsymbolendealer = { "Klaver", "Ruit", "Schoppen", "Harten" };
+            string randomsymbolendealer1 = randomsymbolen[rndmsymb.Next(randomsymbolendealer.Length)];
+            string randomkaartdealer = (randomsymbolendealer1 + " " + rndmnrdealer);
+
+            StringBuilder builder= new StringBuilder();
+            var newstringbuilder = builder;
+            builder.Append(randomkaart + Space + randomkaart0);
+
+            txtSpeler1.Text =(randomkaart + Space + randomkaart0);
+            txtSpeler2.Text = (randomkaartdealer + Space + "Nog onbekend");
+        }
+
+        private void btnHit_Click(object sender, RoutedEventArgs e)
+        {
+            var rndmnr = new Random();
+            int randomn1 = rndmnr.Next(1, 11);
+            string rndmnrummer = Convert.ToString(randomn1);
 
             var rndmsymb = new Random();
             string[] randomsymbolen = { "Klaver", "Ruit", "Schoppen", "Harten" };
             string randomsymbolen1 = randomsymbolen[rndmsymb.Next(randomsymbolen.Length)];
-            string randomkaart = (randomsymbolen1 + " " + rndmnr1);
+            string randomkaart = (randomsymbolen1 + " ");
+                        
+            StringBuilder strng = new StringBuilder(txtSpeler1.Text);
+            var newstrng = strng;
+            newstrng.Append(randomkaart);
+           
+        }
 
-            var rndmsymb0 = new Random();
-            string[] randomsymbolen0 = { "Klaver", "Ruit", "Schoppen", "Harten" };
-            string randomsymbolen01 = randomsymbolen0[rndmsymb0.Next(randomsymbolen0.Length)];
-            string randomkaart0 = (randomsymbolen01 + " " + rndmnr01);
 
-            txtSpeler1.Text = (randomkaart + randomkaart);
-            txtSpeler1.Text = (randomkaart0); 
+        private void btnQuit_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnReset_Click_1(object sender, RoutedEventArgs e)
+        {
+            txtSpeler1.Text = string.Empty;
+            txtSpeler2.Text = string.Empty;
+            lblBankScore.Content = "0";
+            lblSpelerScore.Content = "0";
+            lblTotaalBank.Content = "0";
+            lblTotaalSpeler.Content = "0";
         }
     }
 }
